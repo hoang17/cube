d9a0335f330c39d42d861e0a11cc38ce78281cf3
Hoang Le <lehuyhoang117@gmail.com>
2017-05-17 17:54:17 +0700

    add bluebird

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,16 +1,24 @@
 const bluebird = require('bluebird');
 const request = bluebird.promisifyAll(require('request'), { multiArgs: true });
-const graph = require('fbgraph');
+const graph = bluebird.promisifyAll(require('fbgraph'));
 
 exports.index = (req, res) => {
   const token = req.user.tokens.find(token => token.kind === 'facebook');
   graph.setVersion("2.3");
   graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}/groups?limit=200`, (err, results) => {
-    if (err) { return next(err); }
+
+  // graph.get(`${req.user.facebook}/groups?limit=200`, (err, results) => {
+  //   if (err) { return next(err); }
+  //   res.render('groups', {
+  //     title: 'Groups',
+  //     groups: results.data
+  //   });
+  // });
+
+  graph.getAsync(`${req.user.facebook}/groups?limit=200`).then(results => {
     res.render('groups', {
       title: 'Groups',
       groups: results.data
     });
-  });
+  })
 };
