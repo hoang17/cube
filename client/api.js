0d985833e928f2e7c148d367c00c21154fb1483d
Hoang Le <lehuyhoang117@gmail.com>
2017-05-28 00:36:02 +0700

    fix api host

--- client/api.js
+++ client/api.js
@@ -2,10 +2,12 @@ import axios from 'axios'
 
 const port = process.env.PORT || 3000;
 
+const host = process.env.VUE_ENV === 'server' ? `http://localhost:${port}` : window.location.origin
+
 export function getGroups (){
-  return axios.get(`http://localhost:${port}/api/groups`).then(response => { return response.data })
+  return axios.get(`${host}/api/groups`).then(response => { return response.data })
 }
 
 export function getLikes (){
-  return axios.get(`http://localhost:${port}/api/likes`).then(response => { return response.data })
+  return axios.get(`${host}/api/likes`).then(response => { return response.data })
 }
