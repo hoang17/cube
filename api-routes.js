059498c0e2848a039726bcc0bf60c28a3371b42c
Hoang Le <lehuyhoang117@gmail.com>
2017-05-28 23:33:23 +0700

    move api and ssr routes to new files

---
+++ api-routes.js
@@ -0,0 +1,31 @@
+const Group = require('./models/Group')
+const Like = require('./models/Like')
+const router = require('express').Router()
+
+router.route('/groups')
+  .get(function(req, res) {
+    Group.find().lean().exec(function(err, groups) {
+      if (err)
+        res.send(err)
+      res.json(groups)
+    })
+  })
+  // .post(function(req, res) {
+  //   var group = new Group()
+  //   group.name = req.body.name
+  //   group.save(function(err) {
+  //     if (err)
+  //       res.send(err);
+  //     res.json({ message: 'Group created!' });
+  //   })
+  // })
+router.route('/likes')
+  .get(function(req, res) {
+    Like.find().lean().exec(function(err, likes) {
+      if (err)
+        res.send(err)
+      res.json(likes)
+    })
+  })
+
+module.exports = router
