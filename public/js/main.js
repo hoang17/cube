f4e74fe2d4fa9e877f644aa9f891ece13c314b3c
Hoang Le <lehuyhoang117@gmail.com>
2017-05-31 17:32:02 +0700

    auto hide nav bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 
-  var prev = 0, offset = $('.navbar').height()
+  // var prev = 0, offset = $('.navbar').height()
 
   $(window).scroll(function () {
-    var cur = $(this).scrollTop()
-    if (cur > offset) {
-      if (cur > prev)
-        $('.navbar').fadeOut()
-      else
-        $('.navbar').fadeIn()
-    }
-    prev = cur
-    
-    // if ($(this).scrollTop() > 100) {
+
+    // var cur = $(this).scrollTop()
+    // if (cur > offset) {
+    //   if (cur > prev)
     //     $('.navbar').fadeOut()
-    // } else {
+    //   else
     //     $('.navbar').fadeIn()
     // }
+    // prev = cur
+
+    if ($(this).scrollTop() > 100) {
+        $('.navbar').fadeOut()
+    } else {
+        $('.navbar').fadeIn()
+    }
   })
 })
