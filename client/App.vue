40d0d308e1180ebc9c81ddeff4d76f5c3c70c1e4
Hoang Le <lehuyhoang117@gmail.com>
2017-08-17 14:10:57 +0700

    clean up

--- client/App.vue
+++ client/App.vue
@@ -21,8 +21,9 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-body
-  /*font-family -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+
+/*body
+  font-family -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
   font-size 15px
   background-color lighten(#eceef1, 30%)
   margin 0
