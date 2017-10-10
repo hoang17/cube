0b171e3910e29301a1e5df7d259a61e2ada8e63b
Hoang Le <lehuyhoang117@gmail.com>
2017-10-10 13:23:36 +0700

    no message

--- build/version.js
+++ build/version.js
@@ -1,5 +1,6 @@
-const childProcess = require('child_process')
 module.exports = function(){
-  let n = childProcess.execSync('git rev-list HEAD --count').toString()
-  return childProcess.execSync('git describe --tags --long').toString().trim() + '-' + n
+  return require('../package.json').version
+  // const childProcess = require('child_process')
+  // let n = childProcess.execSync('git rev-list HEAD --count').toString()
+  // return childProcess.execSync('git describe --tags --long').toString().trim() + '-' + n
 }
