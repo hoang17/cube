530defde91e1227665d511377f19bc7e12cff852
Hoang Le <lehuyhoang117@gmail.com>
2017-10-11 03:09:24 +0700

    gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -7,10 +7,3 @@ git push bb --all
 git push gitlab --all
 git push github --all
 git push bitbucket --all
-
-git push bb --tags
-git push gitlab --tags
-git push github --tags
-git push bitbucket --tags
-
-npm run up
