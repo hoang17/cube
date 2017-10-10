9f8f2b978e38320effc2f04e362dc63bc95ae813
Hoang Le <lehuyhoang117@gmail.com>
2017-10-11 03:12:58 +0700

    pipe

--- gitpush.sh
+++ gitpush.sh
@@ -3,7 +3,4 @@
 #echo on
 set -x
 
-git push bb --all
-git push gitlab --all
-git push github --all
-git push bitbucket --all
+git push bb --all | git push gitlab --all | git push github --all | git push bitbucket --all
--- patch.sh
+++ patch.sh
@@ -3,14 +3,8 @@
 #echo on
 set -x
 
-git push bb --all
-git push gitlab --all
-git push github --all
-git push bitbucket --all
+git push bb --all | git push gitlab --all | git push github --all | git push bitbucket --all
 
-git push bb --tags
-git push gitlab --tags
-git push github --tags
-git push bitbucket --tags
+git push bb --tags | git push gitlab --tags | git push github --tags | git push bitbucket --tags
 
 npm run up
