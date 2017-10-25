eb7b56347da02740295a2c5c779d6a84ec7365a8
Hoang Le <lehuyhoang117@gmail.com>
2017-10-25 12:46:44 +0700

    button

--- client/cubes/Button.vue
+++ client/cubes/Button.vue
@@ -30,9 +30,18 @@ export default {
 
 <style lang="stylus" module>
 .button
-  outline none
   composes cube from "./cube.css"
+  outline none
+  background none
+  border 1px solid rgba(0,0,0,.15)
+  border-radius 0
+  cursor pointer
   display inline-block
+  box-shadow 0 1px 2px rgba(0,0,0,0.1)
+  &:hover
+    border 1px solid rgba(0,0,0,.3)
+  &:active
+    box-shadow 0 2px 2px rgba(0,0,0,0.1)
   &[edit]
     cursor pointer
 </style>
