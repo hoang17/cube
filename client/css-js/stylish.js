802e7d5da2d8c0fedfd1be4f1555f290c4ebb75c
Hoang Le <lehuyhoang117@gmail.com>
2017-10-28 12:38:28 +0700

    stylish.js murmurhash-js

---
+++ client/css-js/stylish.js
@@ -0,0 +1,59 @@
+import {create} from 'jss'
+import preset from 'jss-preset-default'
+import hash from 'murmurhash-js/murmurhash3_gc'
+
+const meta = 'css'
+const isNotFalsy = val => !!val
+const getClassName = rule => rule.className
+const generateClassName = (name, str) => `${name}-${hash(name + str + meta)}`
+const mergeStyles = (style, rule) => ({...style, ...rule.style})
+
+export default function stylish(jss, options) {
+  const renderSheet = () => (
+    jss.createStyleSheet(null, {meta, ...options}).attach()
+  )
+
+  let sheet = renderSheet()
+
+  function css(...rules) {
+    // Filter falsy values to allow `css(a, test && c)`.
+    rules = rules.filter(isNotFalsy)
+
+    if (!rules.length) return ''
+
+    // A joined class name from all rules.
+    const className = rules.map(getClassName).join('--')
+
+    if (sheet.getRule(className)) return className
+
+    const style = rules.reduce(mergeStyles, {})
+    sheet.addRule(className, style, {selector: `.${className}`})
+
+    return className
+  }
+
+  function register(styles) {
+    return Object.keys(styles).reduce((map, name) => {
+      map[name] = {
+        className: generateClassName(name, JSON.stringify(styles[name])),
+        style: styles[name]
+      }
+      return map
+    }, {})
+  }
+
+  function reset() {
+    jss.removeStyleSheet(sheet)
+    sheet = renderSheet()
+  }
+
+  return {
+    StyleSheet: {create: register},
+    toString: () => sheet.toString(),
+    css,
+    reset,
+    version: __VERSION__
+  }
+}
+
+export const {css, StyleSheet, reset, toString, version} = aphroditeJss(create(preset()))
