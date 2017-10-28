f5f0d1a09c38d5f201b527e3d457199b08e8b91c
Hoang Le <lehuyhoang117@gmail.com>
2017-10-29 03:14:23 +0700

    no message

--- client/components/Field.vue
+++ client/components/Field.vue
@@ -12,53 +12,10 @@
 
 <script>
 import { css } from 'emotion'
-import cxs from 'cxs'
-import { createRenderer } from 'fela'
-import { render } from 'fela-dom'
 
 const myStyle = css`
-  color: red
+  color: tomato
 `
-
-const cxsClass = cxs({
-  color: 'blue',
-  ':hover': {
-    color: 'tomato'
-  }
-})
-
-// rules are just plain functions of props
-// returning a valid object of style declarations
-const rule = props => ({
-  fontSize: props.fontSize + 'px',
-  marginTop: props.margin ? '15px' : 0,
-  color: 'red',
-  lineHeight: 1.4,
-  ':hover': {
-    color: 'blue',
-    fontSize: props.fontSize + 2 + 'px'
-  },
-  // nest media queries and pseudo classes
-  // inside the style object
-  '@media (min-height: 300px)': {
-    backgroundColor: 'gray',
-    ':hover': {
-      color: 'black'
-    }
-  }
-})
-
-// creates a new renderer to render styles
-const renderer = createRenderer()
-// rendering the rule returns a className reference
-// which can be attached to any element
-const className = renderer.renderRule(rule, { fontSize: 12 })
-// it uses atomic css design to reuse styles
-// on declaration base and to keep the markup minimal
-console.log(className) // => a b c d e f h
-// renders all styles into the DOM
-render(renderer)
-
 
 import Box from './Box'
 import Input from './Input'
@@ -73,7 +30,7 @@ export default {
   },
   data(){
     return {
-      myStyle: cxsClass
+      myStyle: myStyle
     }
   }
 }
