328fbe81d220f5673da4854398732a863cb6d5bb
Hoang Le <lehuyhoang117@gmail.com>
2017-11-01 15:10:08 +0700

    Background

--- client/components/Background.vue
+++ client/components/Background.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   div
-    //-FieldSet
+    FieldSet
+      Field(lb="Background" v-model='rule.background')
+    //- FieldSet
       SelectField(lb="Style" v-model='rule.backgroundStyle')
         option(value="color") Color
         option(value="gradient") Gradient
@@ -8,51 +10,58 @@
         option(value="video") Video
     FieldSet
       ColorPicker(lb="Color" v-model='rule.backgroundColor')
-    //-FieldSet
-      Field(lb="Gradient")
-    //-FieldSet
-      SelectField(lb="Select Image" v-model='rule.backgroundImg')
-        option Light
-        option Thin
-        option Normal
-        option Bold
-        option Heavy
-    //-FieldSet
+    FieldSet
+      Field(lb="Background Image URL" v-model='bgImg' ph="URL")
+    FieldSet
       SelectField(:w="1/3" lb="Position" v-model='rule.backgroundPosition')
         option(value='center') Center
-        option(value='top-left') Top Left
         option(value='top') Top
-        option(value='top-right') Top right
-        option(value='right') Right
-        option(value='bottom-right') Bottom Right
         option(value='bottom') Bottom
-        option(value='bottom-left') Bottom Left
         option(value='left') Left
+        option(value='right') Right
+        option(value='top left') Top Left
+        option(value='top right') Top right
+        option(value='bottom left') Bottom Left
+        option(value='bottom right') Bottom Right
       SelectField(:w="1/3" lb="Repeat" v-model='rule.backgroundRepeat')
-        option(value='off') Off
-        option(value='horizontal') Horizontally
-        option(value='vertical') Vertically
-        option(value='both') Both
-      SelectField(:w="1/3" lb="Size" v-model='rule.backgroundSize')
-        option(value='fill') Fill
-        option(value='fit') Fit
-        option(value='stretch') Stretch
-        option(value='default') Default
+        option(value='no-repeat') Off
+        option(value='repeat-x') Horizontally
+        option(value='repeat-y') Vertically
+        option(value='repeat') Both
+        option(value='space') Space
+        option(value='round') Round
+      SelectField(:w="1/3" lb="Attachment" v-model='rule.backgroundAttachment')
+        option(value='scroll') Scroll
+        option(value='fixed') Fixed
+        option(value='local') Local
+      //- InputSelectField(:w="1/3" lb="Size" v-model='rule.backgroundSize')
+        option(value='cover') Cover
+        option(value='contain') Contain
+        option(value='100% 100%') Stretch
         option(value='custom') Custom
+    FieldSet
+      Field(lb="Size" v-model='rule.backgroundSize')
 </template>
 <script>
 import Field from './Field'
 import FieldSet from './FieldSet'
 import SelectField from './SelectField'
 import ColorPicker from './ColorPicker'
+import parseCssUrls from 'css-url-parser'
 
 export default {
   props:['rule'],
   components: {
     Field, FieldSet, SelectField, ColorPicker
   },
-  data(){
-    return {
+  computed: {
+    bgImg:{
+      get(){
+        return this.rule.backgroundImage ? parseCssUrls(this.rule.backgroundImage)[0] : ''
+      },
+      set(val){
+        this.rule.backgroundImage = `url(${val})`
+      }
     }
   }
 }
