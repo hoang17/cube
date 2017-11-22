d18e307c3fb15fd66d07374b27e1427386294588
Hoang Le <lehuyhoang117@gmail.com>
2017-11-22 17:58:43 +0700

    StripeButton

--- client/cubes/StripeButton.vue
+++ client/cubes/StripeButton.vue
@@ -8,7 +8,9 @@
     form(:action="cube.url", method="post")
       div(:class="$style.amount") {{ cube.content }}
       input(type='hidden' name='handle' :value="cube.handle")
-      script(
+      button(type="submit" @click.prevent="checkout" :class="$style.stripeButton")
+        span Pay with Card
+      //- script(
         src="//checkout.stripe.com/checkout.js"
         class="stripe-button"
         data-name="Cube"
@@ -35,7 +37,26 @@ export default {
       ]
     },
   },
+  mounted() {
+    this.handler = StripeCheckout.configure({
+      key: 'pk_test_QMK3N95W3aCt9eydN1zUzFE8',
+      image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+      locale: 'auto',
+      token: function(token) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+      }
+    })
+  },
   methods: {
+    checkout(){
+      this.handler.open({
+        name: 'Cube',
+        amount: parseInt(this.cube.amount),
+        panelLabel: this.cube.panelLabel,
+        description: this.cube.description,
+      })
+    },
     onClick(){
       if (this.edit)
         this.select(this.cube, this.$el, this.parent)
@@ -59,4 +80,35 @@ export default {
   padding .6em 1.7em
   &[edit]
     cursor pointer
+
+.stripeButton
+  overflow: hidden
+  display: inline-block
+  visibility: visible !important
+  background-image: linear-gradient(#28a0e5,#015e94)
+  -webkit-font-smoothing: antialiased
+  border: 0
+  padding: 1px
+  text-decoration: none
+  border-radius: 5px
+  box-shadow: 0 1px 0 rgba(0,0,0,0.2)
+  user-select: none
+  cursor: pointer
+
+  span
+    display: block
+    min-height: 30px
+    position: relative
+    padding: 0 12px
+    height: 30px
+    line-height: 30px
+    background: #1275ff
+    background-image: linear-gradient(#7dc5ee,#008cdd 85%,#30a2e4)
+    font-size: 14px
+    color: #fff
+    font-weight: bold
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif
+    text-shadow: 0 -1px 0 rgba(0,0,0,0.25)
+    box-shadow: inset 0 1px 0 rgba(255,255,255,0.25)
+    border-radius: 4px
 </style>
