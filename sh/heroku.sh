0e25a0a124a759e97dad2111774162a7437b3d3a
Hoang Le <lehuyhoang117@gmail.com>
2017-12-07 22:21:29 +0700

    no message

--- sh/heroku.sh
+++ sh/heroku.sh
@@ -9,4 +9,4 @@ git commit -am 'new deployment'
 git remote add heroku https://git.heroku.com/dayflow.git
 git push heroku master --force
 open http://yah.vn/build
-echo "Code deployed to heorku server"
+echo "Code deployed to heroku server"
